11885caa13c213344448e7eea9be0032c5f98222
AlexVipond <alex.r.vipond@gmail.com>
2020-07-31 21:55:54 -0600

    Support more generic file tests and async transforms

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import pluginUtils from '@rollup/pluginutils'
 
+const { createFilter } = pluginUtils
+
 export default function sourceTransform (options = {}) {
-  const { transform, include, exclude } = options,
-        { createFilter } = pluginUtils,
-        filter = createFilter(include, exclude)
+  const { transform, include, exclude, test: rawTest } = options,
+        test = resolveTest(include, exclude, rawTest)
 
   return {
     name: 'source-transform',
-    transform: (source, id) => {
-      if (!filter(id)) {
+    async transform (source, id) {
+      if (!test({ source, id, createFilter })) {
         return null
       }
 
-      return transform({
+      return await transform({
         source,
         id,
         context: this,
@@ -20,4 +21,10 @@ export default function sourceTransform (options = {}) {
       })
     }
   }
+}
+
+function resolveTest (include, exclude, test) {
+  return typeof test === 'function'
+    ? test
+    : ({ id, createFilter }) => createFilter(include, exclude)(id)
 }
